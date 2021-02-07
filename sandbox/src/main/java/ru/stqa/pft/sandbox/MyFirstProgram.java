9d71b84ec36186f37621a296db6e8c0c30b244ed
AltaAltynai <a.kanatpayeva@gmail.com>
2021-02-07 22:35:19 +0600

    выполнение задания 1
    - перенос запуска кода в MyFirst Prgram класс
    - практика использования конструктора

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Point p = new Point(2,3,4,5);
+
+        System.out.println("Расстояние на плоскости между точками " +
+                "Point1 " + "(" + p.x1 + "," + p.y1 + ")" + " и Point2 " + "(" + p.x2 + "," + p.y2 + ")" +
+                " = " + distance(p));
     }
 
+    public static double distance(Point p) {
+        return  Math.sqrt((p.y2 - p.y1) * (p.y2 - p.y1) + (p.x2 - p.x1) *  (p.x2 - p.x1));
+    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,18 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-    public static void main(String[] args) {
 
-        double x1 = 3;
-        double y1 = 8;
-        double x2 = 4;
-        double y2 = 9;
-        System.out.println("Расстояние на плоскости между точками " +
-                "Point1 " + "(" + x1 + "," + y1 + ")" + " и Point2 " + "(" + x2 + "," + y2 + ")" +
-                " = " + distance(x1, y1, x2, y2));
-    }
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
 
-    public static double distance(double x1, double y1, double x2, double y2) {
-        return  Math.sqrt((y2 - y1) *  (y2 - y1) + (x2 - x1) *  (x2 - x1));
+    public Point (double x1 , double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 }
