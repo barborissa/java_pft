966f1032e37e100199bbe8570527f8ae071ee231
AltaAltynai <a.kanatpayeva@gmail.com>
2021-02-08 08:35:29 +0600

    задание 1 - добавление метода в классе point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,15 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Point p1 = new Point(1,0);
-        Point p2 = new Point(0,0);
-
+        Point p1 = new Point(-3,3);
+        Point p2 = new Point(4,-2);
         System.out.println("Расстояние на плоскости между точками " +
-                "Point1 " + "(" + p1.x + "," + p1.y + ")" + " и Point2 " + "(" + p2.x + "," + p2.y + ")" +
-                " = " + distance(p1, p2));
-    }
+                "Point 1 " + "(" + p1.x + "," + p1.y + ")" + " и Point 2 " + "(" + p2.x + "," + p2.y + ")" +
+                " = " + p1.distance(p2));
 
-    public static double  distance(Point p1, Point p2) {
-        return  Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) *  (p2.y - p1.y));
     }
 }
