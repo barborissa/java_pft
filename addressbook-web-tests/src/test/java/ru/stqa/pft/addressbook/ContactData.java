96fc5f146f32826a010b10444897bf5179c06f78
AltaAltynai <a.kanatpayeva@gmail.com>
2021-02-16 08:35:46 +0600

    Задание 4 - добавлены тесты для создания контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String surname;
+    private final String address;
+    private final String mobile;
+    private final String bday;
+    private final String bmonth;
+
+    public ContactData(String firstname, String surname, String address, String mobile, String bday, String bmonth) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.address = address;
+        this.mobile = mobile;
+        this.bday = bday;
+        this.bmonth = bmonth;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getBmonth() {
+        return bmonth;
+    }
+}
